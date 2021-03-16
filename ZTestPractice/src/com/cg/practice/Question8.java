47f74108afc5d0d66dfed92c67720443b557f6fd
SHIVENDRAIYER <shivendrav13@gmail.com>
2021-03-16 10:26:40 +0530

    AdditionalPractice 1-10

---
+++ ZTestPractice/src/com/cg/practice/Question8.java
@@ -0,0 +1,33 @@
+package com.cg.practice;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+
+		int a[] = { 1, 2, 3, 4, 5 };
+		int b[] = { 6, 7, 8, 9, 10 };
+		int c[] = alternativeIndicesElements(a, b);
+
+		for (int d : c)
+			System.out.println(d);
+	}
+
+	public static int[] alternativeIndicesElements(int[] a, int[] b) {
+
+		int c[] = new int[a.length];
+		if (a.length == b.length) {
+
+			for (int i = 0; i < a.length; i++) {
+
+				if (i % 2 == 0) {
+
+					c[i] = b[i];
+				} else {
+
+					c[i] = a[i];
+				}
+			}
+		}
+		return c;
+	}
+}
